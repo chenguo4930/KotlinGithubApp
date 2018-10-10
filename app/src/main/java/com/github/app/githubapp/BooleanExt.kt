514d0c924f2ffe51f188eaaecee339cada419d74
cheng <chengguo4930@hotmail.com>
2018-10-11 00:05:20 +0800

    测试tag

--- app/src/main/java/com/github/app/githubapp/BooleanExt.kt
+++ app/src/main/java/com/github/app/githubapp/BooleanExt.kt
@@ -1,24 +1,5 @@
 package com.github.app.githubapp
 
-fun main(args: Array<String>) {
-
-    val result = if (getABoolean()) {
-        1
-    } else {
-        2
-    }
-
-    val result2 = getABoolean().yes {
-        1
-    }.no {
-        2
-    }
-
-    println("result2 = $result2")
-}
-
-fun getABoolean() = false
-
 sealed class BooleanExt<out T>
 
 object Otherwise : BooleanExt<Nothing>()
